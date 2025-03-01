11f854d162c5e32df292d8f88327bb0462c467bf
dhncfrnndz <dhanicafearl.fernandez@lorma.edu>
2025-03-01 15:42:26 +0800

    Initial commit

---
+++ dhan_bssnscrdp/android/app/src/main/kotlin/com/dhncfrnndz/dhan_bssnscrdp/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.dhncfrnndz.dhan_bssnscrdp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
--- dhan_bussinesscardapp/android/app/src/main/kotlin/com/dhncfrnndz/dhan_bussinesscardapp/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.dhncfrnndz.dhan_bussinesscardapp
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
